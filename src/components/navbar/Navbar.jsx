20ede147167c4476b1628b980f702b353ddea118
yousefkassab99 <105107425+yousefkassab99@users.noreply.github.com>
2022-10-06 19:31:41 +0300

    Hotels-frontEnd

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import "./navbar.css"
+
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="navContainer">
+        <span className="logo">lamabooking</span>
+        <div className="navItems">
+         <button className="navButton">Register</button>
+         <button className="navButton">login</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
