a1ad5ed97ea0f9ece25dcdbbe22f0743f58ec07c
yousefkassab99 <105107425+yousefkassab99@users.noreply.github.com>
2022-10-24 16:49:39 +0300

    Hotels-FrontEnd

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,29 +1,28 @@
-import Featured from "../../components/featured/Featured"
-import FeaturedProperties from "../../components/featuredProperties/FeaturedProperties"
-import Footer from "../../components/footer/Footer"
-import Header from "../../components/header/Header"
-import MailList from "../../components/mailList/MailList"
-import Navbar from "../../components/navbar/Navbar"
-import PropertyList from "../../components/propertyList/PropertyList"
-import "./home.css"
+import Featured from "../../components/featured/Featured";
+import FeaturedProperties from "../../components/featuredProperties/FeaturedProperties";
+import Footer from "../../components/footer/Footer";
+import Header from "../../components/header/Header";
+import MailList from "../../components/mailList/MailList";
+import Navbar from "../../components/navbar/Navbar";
+import PropertyList from "../../components/propertyList/PropertyList";
+import "./home.css";
 
-
-function Home() {
+const Home = () => {
   return (
     <div>
-     <Navbar />
-     <Header />
-     <div className="homeContainer">
+      <Navbar />
+      <Header/>
+      <div className="homeContainer">
         <Featured/>
-        <h1 className="homeTitle">Brows by property type</h1>
+        <h1 className="homeTitle">Browse by property type</h1>
         <PropertyList/>
-        <h1 className="homeTitle">Home guests love</h1>
-         <FeaturedProperties/>
-         <MailList/>
-        <Footer />
-     </div>
+        <h1 className="homeTitle">Homes guests love</h1>
+        <FeaturedProperties/>
+        <MailList/>
+        <Footer/>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
